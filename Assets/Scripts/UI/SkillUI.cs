d90f77fe2857f905cf3c8eb0fe8baeb28ca56cfb
thanh.truong <thanh.truongcong2002@gmail.com>
2024-03-13 07:00:06 +0700

    Update todo list + fix skill 2

--- Assets/Scripts/UI/SkillUI.cs
+++ Assets/Scripts/UI/SkillUI.cs
@@ -6,20 +6,20 @@ using UnityEngine.UI;
 public class SkillUI : MonoBehaviour
 {
     [SerializeField] GameObject P1Skill;
-    [SerializeField] GameObject P2Skill;
+    // [SerializeField] GameObject P2Skill;
     public void SetUp(){
         P1Skill.GetComponent<Toggle>().onValueChanged.AddListener((bool value)=>{
             GameManager.instance.battleHandler.GetCharacterBattle1().GetComponent<Monster>().SetIsSkill(value);
         });
-        P2Skill.GetComponent<Toggle>().onValueChanged.AddListener((bool value)=>{
-            GameManager.instance.battleHandler.GetCharacterBattle2().GetComponent<Monster>().SetIsSkill(value);
-        });
+        // P2Skill.GetComponent<Toggle>().onValueChanged.AddListener((bool value)=>{
+        //     GameManager.instance.battleHandler.GetCharacterBattle2().GetComponent<Monster>().SetIsSkill(value);
+        // });
         // P1Skill.GetComponent<Toggle>().interactable = false;
         // P2Skill.GetComponent<Toggle>().interactable = false;
-        P2Skill.GetComponent<Toggle>().onValueChanged.AddListener((bool value)=>{
-            GameManager.instance.battleHandler.GetCharacterBattle2().GetComponent<Monster>().SetIsSkill(value);
-        });
+        // P2Skill.GetComponent<Toggle>().onValueChanged.AddListener((bool value)=>{
+        //     GameManager.instance.battleHandler.GetCharacterBattle2().GetComponent<Monster>().SetIsSkill(value);
+        // });
         P1Skill.GetComponent<Image>().sprite = GameManager.instance.battleHandler.GetCharacterBattle1().GetComponent<Monster>().GetSkillBase().icon;
-        P2Skill.GetComponent<Image>().sprite = GameManager.instance.battleHandler.GetCharacterBattle2().GetComponent<Monster>().GetSkillBase().icon;
+        // P2Skill.GetComponent<Image>().sprite = GameManager.instance.battleHandler.GetCharacterBattle2().GetComponent<Monster>().GetSkillBase().icon;
     }
 }
